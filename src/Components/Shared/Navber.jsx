e22868b18ee5742d9d9b8e7492b3a7e6d3866ab9
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-11-07 12:32:18 +0600

    Implemented codes for ErrorPage and set the Error Element Route

--- src/Components/Shared/Navber.jsx
+++ src/Components/Shared/Navber.jsx
@@ -39,9 +39,7 @@ const Navber = () => {
                             </div>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <Link>Profile</Link>
-                            </li>
+                            <li><Link>Profile</Link></li>
                             <li><Link>Logout</Link></li>
                         </ul>
                     </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import HomePage from "../Pages/HomePage";
 import Bookings from "../Pages/Bookings";
+import ErrorPage from "../Pages/ErrorPage";
 
 
 const Router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout></MainLayout>,
+        errorElement:<ErrorPage></ErrorPage>,
         children:[
             {
                 path:"/",
